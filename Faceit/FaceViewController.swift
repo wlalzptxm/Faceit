4b0b431eeacd84a4bd3d53531c1c86812819efe2
wlalzptxm <wlalzptx11@gmail.comm>
2018-08-23 16:33:42 +0900

    getting start

---
+++ Faceit/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Seunghoon Jeon on 23/08/2018.
+//  Copyright © 2018 Seunghoon Jeon. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+
+
+}
+
