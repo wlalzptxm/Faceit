312fd16a20a0d9659aee0955cb712f32ee7a4082
wlalzptxm <wlalzptx11@gmail.comm>
2018-08-27 15:36:13 +0900

    error-fixed
    プロパティに対する初期化エラー、
    基本顔完成。

--- Faceit/FaceView.swift
+++ Faceit/FaceView.swift
@@ -9,19 +9,102 @@
 import UIKit
 
 class FaceView: UIView {
+    
+    var mouthCurvature: Double = -1.0 // 1 full smile, -1 full frown
+    var scale: CGFloat = 0.90
+    
+    // 描いてくれる頭の半径を取るメソッド
+    // boundsを活用して基準点で握る(skullCenter)部分から描くことができるようにするために
+    // エラーが出る理由はskullRadius初期化するうちにboundsような変数やメソッドを呼ぶことができないからな
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+    
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    // こんなふうに演算プロパティで作成して使用しなければならない、またいつもプロパティは、初期化をして書けばいいためだ
+    // ただget(readonly)のみ使用するなら、省略することができる
+    
+    // 構造体を宣言する方法、大文字を使うという点が注目する
+    private struct Ratios {
+        // staticらは構造体で基本的に定数であるために
+        static let SkullRadiusToEyeOffset: CGFloat = 3
+        static let SkullRadiusToEyeRadius: CGFloat = 10
+        static let SkullRadiusToMouthWidth: CGFloat = 1
+        static let SkullRadiusToMouthHeight: CGFloat = 3
+        static let SkullRadiusToMouthOffset: CGFloat = 3
+        static let SkullRadiusToBrowOffset: CGFloat = 5
+    }
+    
+    private enum Eye {
+        case Left
+        case Right
+    }
+    
+    private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath{
+        let path = UIBezierPath(
+            arcCenter: midPoint,
+            radius: radius,
+            startAngle: 0.0,
+            endAngle: CGFloat(2*Double.pi),
+            clockwise: false
+        )
+        path.lineWidth = 5.0
+        return path
+    }
+    
+    private func getEyeCenter(eye: Eye) -> CGPoint {
+        let eyeOffset = skullRadius / Ratios.SkullRadiusToEyeOffset
+        var eyeCenter = skullCenter
+        eyeCenter.y -= eyeOffset
+        switch eye {
+        case .Left: eyeCenter.x -= eyeOffset
+        case .Right: eyeCenter.x += eyeOffset
+        }
+        return eyeCenter
+    }
+    
+    // 目を描く時skullRadius、skullCenterのみ使用して描くということに注目する。
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
+        let eyeCenter = getEyeCenter(eye: eye)
+        
+        return pathForCircleCenteredAtPoint(midPoint: eyeCenter, withRadius: eyeRadius)
+    }
+    
+    // 2つの基準点を活用して口を作る
+    private func pathForMouth() -> UIBezierPath
+    {
+        let mouthWidth = skullRadius / Ratios.SkullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.SkullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.SkullRadiusToEyeOffset
+        
+        //口の位置は目のもとに生成することでskullCenter.yを活用する
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth/2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1 = CGPoint(x: mouthRect.minX + mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        let cp2 = CGPoint(x: mouthRect.maxX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
 
-    //그려줄 머리의 반경을 잡는 메소드
-    //bounds를 활용해서 기준점으로 잡는(skullCenter) 부분부터 그릴수 있도록 하기 위해
-    var skullRadius = min(bounds.size.width, bounds.size.height) / 2
-    var skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 5.0
+
+        return path
+    }
     
     override func draw(_ rect: CGRect) {
         
-        //실제로 그려질 대상
-        let skull = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2*Double.pi), clockwise: false)
-        skull.lineWidth = 5.0
-        UIColor.blue
-        skull.stroke()
+        //実際に描かれる対象
+        UIColor.blue.set()
+        pathForCircleCenteredAtPoint(midPoint: skullCenter, withRadius: skullRadius).stroke()
+        pathForEye(eye: .Left).stroke()
+        pathForEye(eye: .Right).stroke()
+        pathForMouth().stroke()
     }
-
+    
 }
